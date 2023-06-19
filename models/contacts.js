ad432c540c610cefe71afb0d967411043fe6e4d7
Алексей <electrifyx@gmail.com>
2023-06-19 18:10:26 +0300

    add homework 2

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,71 @@
-// const fs = require('fs/promises')
+const fs = require("fs/promises");
+const path = require("path");
+const { nanoid } = require("nanoid");
 
-const listContacts = async () => {}
+const contactsPath = path.join(__dirname, "/contacts.json");
+console.log(contactsPath);
 
-const getContactById = async (contactId) => {}
+const listContacts = async () => {
+  try {
+    const data = await fs.readFile(contactsPath);
+    return JSON.parse(data);
+  } catch (error) {
+    return error;
+  }
+};
 
-const removeContact = async (contactId) => {}
+const getContactById = async (id) => {
+  try {
+    const contactId = String(id);
+    const contacts = await listContacts();
+    const results = contacts.find((contact) => contact.id === contactId);
+    return results || null;
+  } catch (error) {
+    return error;
+  }
+};
 
-const addContact = async (body) => {}
+const addContact = async (data) => {
+  try {
+    const contacts = await listContacts();
+    const newContact = { id: nanoid(), ...data };
+    contacts.push(newContact);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+    return newContact;
+  } catch (error) {
+    return error;
+  }
+};
 
-const updateContact = async (contactId, body) => {}
+const updateContact = async (id, data) => {
+  try {
+    const contacts = await listContacts();
+    const index = contacts.findIndex((contact) => contact.id === id);
+    if (index === -1) {
+      return null;
+    }
+    contacts[index] = { id, ...data };
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+    return contacts[index];
+  } catch (error) {
+    return error;
+  }
+};
+
+const removeContact = async (id) => {
+  try {
+    const contacts = await listContacts();
+    const index = contacts.findIndex((contact) => contact.id === id);
+    if (index === -1) {
+      return null;
+    }
+    const [result] = contacts.splice(index, 1);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+    return result;
+  } catch (error) {
+    return error;
+  }
+};
 
 module.exports = {
   listContacts,
@@ -16,4 +73,4 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-}
+};
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const app = require('./app')
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
+app.listen(777, () => {
+  console.log("Server running. Port: 777")
 })
