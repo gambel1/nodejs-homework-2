165ab9498811999b5a47233df71fd636f6e3a681
Алексей <electrifyx@gmail.com>
2023-06-27 14:21:36 +0300

    add new branch

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
-const app = require('./app')
+const app = require("./app");
+const mongoose = require("mongoose");
 
-app.listen(777, () => {
-  console.log("Server running. Port: 777")
-})
+const { DB_HOST } = require("./config");
+
+mongoose.set("strictQuery", true);
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(777, () => {
+      console.log("Server running. Port: 777");
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
